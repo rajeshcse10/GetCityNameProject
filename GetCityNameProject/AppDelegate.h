d7954084cae1ece0c79c8ba38362d74eea51076b
Rajesh Karmaker <rajeshkarmaker0@gmail.com>
2017-12-03 11:54:52 +0600

    Initial Commit

---
+++ GetCityNameProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetCityNameProject
+//
+//  Created by Rajesh's MacBook Pro  on 12/3/17.
+//  Copyright © 2017 MacBook Pro Retina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GetCityNameProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetCityNameProject
+//
+//  Created by Rajesh's MacBook Pro  on 12/3/17.
+//  Copyright © 2017 MacBook Pro Retina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
