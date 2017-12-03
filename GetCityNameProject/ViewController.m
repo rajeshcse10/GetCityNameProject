e4440faec7afde16b157392788cc78108acc8ddc
Rajesh Karmaker <rajeshkarmaker0@gmail.com>
2017-12-03 12:25:33 +0600

    First commit

--- GetCityNameProject/ViewController.m
+++ GetCityNameProject/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#define URL(lat,lon) [NSString stringWithFormat:@"https://maps.googleapis.com/maps/api/geocode/json?latlng=%f,%f&sensor=false&key=AIzaSyBHE7RslaK3kI9h041US0_Y9101cJcUZ_0",lat,lon];
 @interface ViewController ()
 
 @end
@@ -16,14 +16,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self getCityFromLatitude:36.106016 Longitude:128.412466];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-
+-(void) getCityFromLatitude:(double) latitude Longitude:(double) longitude{
+    __block NSString *cityName = nil;
+    NSString *urlString = URL(latitude, longitude);
+    [[[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:urlString]completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if(error == nil){
+            NSError *jsonError;
+            NSDictionary *json = (NSDictionary *)[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&jsonError];
+            NSArray *resultList = (NSArray *) [json objectForKey:@"results"];
+            if(resultList.count > 0){
+                NSDictionary *dict = [resultList objectAtIndex:0];
+                NSArray *addressCompList = [dict objectForKey:@"address_components"];
+                if(addressCompList.count > 2){
+                    cityName = [[addressCompList objectAtIndex:2] objectForKey:@"long_name"];
+                }
+            }
+            
+        }
+        if(cityName == nil){
+            NSLog(@"NO CITY NAME FOUND");
+        }
+        else{
+            NSLog(@"CityName : %@",cityName);
+        }
+    }] resume];
+}
 @end
