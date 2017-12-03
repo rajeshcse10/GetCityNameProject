b43915ff8b6f9896c9fc3e68fc79e1c528324916
Rajesh Karmaker <rajeshkarmaker0@gmail.com>
2017-12-03 21:19:03 +0600

    json parsing using stringWithContentOfUrl method

--- GetCityNameProject/ViewController.m
+++ GetCityNameProject/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-#define URL(lat,lon) [NSString stringWithFormat:@"https://maps.googleapis.com/maps/api/geocode/json?latlng=%f,%f&sensor=false&key=AIzaSyBHE7RslaK3kI9h041US0_Y9101cJcUZ_0",lat,lon];
+#define URL(lat,lon) [NSString stringWithFormat:@"https://maps.googleapis.com/maps/api/geocode/json?latlng=%f,%f&sensor=false&key=AIzaSyBHE7RslaK3kI9h041US0_Y9101cJcUZ_0&language=en",lat,lon];
 @interface ViewController ()
 
 @end
@@ -16,7 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self getCityFromLatitude:36.106016 Longitude:128.412466];
+    //[self getCityFromLatitude:36.106016 Longitude:128.412466];
+    [self getCityName];
 }
 
 
@@ -31,6 +32,7 @@
         if(error == nil){
             NSError *jsonError;
             NSDictionary *json = (NSDictionary *)[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&jsonError];
+            NSLog(@"Your JSON Object: %@", json);
             NSArray *resultList = (NSArray *) [json objectForKey:@"results"];
             if(resultList.count > 0){
                 NSDictionary *dict = [resultList objectAtIndex:0];
@@ -48,5 +50,36 @@
             NSLog(@"CityName : %@",cityName);
         }
     }] resume];
+}
+-(void)getCityName{
+    NSString *cityName = nil;
+    NSString *urlString = URL(36.106016,128.412466);
+    NSError *error;
+    NSStringEncoding encoding;
+    NSString *locationString = [NSString stringWithContentsOfURL:[NSURL URLWithString:urlString] usedEncoding:&encoding error:&error];
+    if(error == nil){
+        NSData *data = [locationString dataUsingEncoding:encoding];
+        if(data != nil){
+            NSError *error;
+            NSDictionary *json = (NSDictionary *)[NSJSONSerialization JSONObjectWithData:data options:
+                                                  NSJSONReadingMutableContainers error:&error];
+            NSLog(@"Your JSON Object: %@", json);
+            NSArray *resultList = (NSArray *) [json objectForKey:@"results"];
+            if(resultList.count > 0){
+                NSDictionary *dict = [resultList objectAtIndex:0];
+                NSArray *addressCompList = [dict objectForKey:@"address_components"];
+                if(addressCompList.count > 2){
+                    cityName = [[addressCompList objectAtIndex:2] objectForKey:@"long_name"];
+                }
+            }
+        }
+    }
+    if(cityName == nil){
+        NSLog(@"NO CITY NAME FOUND");
+    }
+    else{
+        NSLog(@"CityName : %@",cityName);
+    }
+
 }
 @end
